6930e4ed252e3e1b8ff5daae77c63fe083eff068
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-05-31 17:11:10 +0000

    create Recipe model, makemigrations & migrate #19

--- collection/models.py
+++ collection/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    excerpt = models.TextField(max_length=1000, blank=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipes"
+    )
+    instructions = models.TextField()
+    prep_time = models.DurationField()
+    cook_time = models.DurationField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=False)
