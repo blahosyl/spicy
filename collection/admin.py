d85d472275a3deb26020631c697571014110b592
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-06-03 20:39:43 +0000

    install summernote #22

--- collection/admin.py
+++ collection/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import Recipe, Ingredient, IngredientQuantity
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
-admin.site.register(Recipe)
+# Register your models here
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', )
+    search_fields = ['title']
+    list_filter = ('author',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('instructions', )
+
+
 admin.site.register(Ingredient)
 admin.site.register(IngredientQuantity)
 
