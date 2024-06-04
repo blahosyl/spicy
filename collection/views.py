229fead23eef2fe6e1babcb55c7a911979515f68
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-06-04 16:33:30 +0000

    add view and template for individual recpie pages #29

--- collection/views.py
+++ collection/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Recipe
 
@@ -7,3 +7,27 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(published=True)
     template_name = "collection/index.html"
     paginate_by = 6
+
+
+def recipe_detail(request, slug):
+    """
+    Display an individual :model:`collection.Recipe`.
+
+    **Context**
+
+    ``recipe``
+        An instance of :model:`collection.Recipe`.
+
+    **Template:**
+
+    :template:`collection/recipe_detail.html`
+    """
+
+    queryset = Recipe.objects.filter(published=True)
+    recipe = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "collection/recipe_detail.html",
+        {"recipe": recipe},
+    )
