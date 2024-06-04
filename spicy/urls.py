f4cff448e376928c6f255954f91978c81c0c9849
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-06-04 17:17:27 +0000

    fix bug: admin unreachable #97

--- spicy/urls.py
+++ spicy/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("collection.urls"), name="collection-urls"),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path("", include("collection.urls"), name="collection-urls"),
 ]
