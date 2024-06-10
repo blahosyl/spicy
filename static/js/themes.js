afff8af0eb5fbfa60b1e9ef4ea5bced37a3993bb
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-06-10 01:47:43 +0000

    update alternative color palette #t98

--- static/js/themes.js
+++ static/js/themes.js
@@ -4,9 +4,14 @@ themeSelector.addEventListener('change',changeTheme);
 
 function changeTheme() {
     if (themeSelector.value == 'green') {
-        bodyStyles.setProperty('--gray', 'green');
+        bodyStyles.setProperty('--dark-highlight', '#1b871d');
+        bodyStyles.setProperty('--highlight', '#7ef075');
+        bodyStyles.setProperty('--light-highlight', '#ceedcc');
+
     }
     else {
-        bodyStyles.setProperty('--gray', '#445261');
+        bodyStyles.setProperty('--dark-highlight', '#D7410F');
+        bodyStyles.setProperty('--highlight', '#FD7E14');
+        bodyStyles.setProperty('--light-highlight', '#f7ddcf');
     }
 }
