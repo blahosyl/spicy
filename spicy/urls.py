2b885bf598416383feaacae5b49a87fb4ab1e8ff
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-06-13 01:35:19 +0000

    add urlpatterns & link to singup template (doesn't work)

--- spicy/urls.py
+++ spicy/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
+    path('accounts/', include('allauth.socialaccount.urls')),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("collection.urls"), name="collection-urls"),
