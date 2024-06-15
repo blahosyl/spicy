784809f2183058034bb9e7038f88b04a8641605d
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-06-15 01:24:05 +0000

    add comment posting functionality #15

--- collection/views.py
+++ collection/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Recipe
+from .forms import CommentForm
+
 
 # Create your views here.
 class RecipeList(generic.ListView):
@@ -30,6 +33,18 @@ def recipe_detail(request, slug):
     ingredient_count = recipe_ingredients.count()
     comments = recipe.comments.all()
     comment_count = recipe.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.recipe = recipe
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+    comment_form = CommentForm()
 
     return render(
         request,
@@ -39,5 +54,6 @@ def recipe_detail(request, slug):
         "ingredient_count": ingredient_count,
         "comments": comments,
         "comment_count": comment_count,
+        "comment_form": comment_form,
         },
     )
