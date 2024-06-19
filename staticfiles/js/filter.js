41af7fb0ea415a4d89628854446c2c660377a13c
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-06-19 04:33:40 +0000

    prep for deployment
    - collect static files
    - DEBUG=False

--- staticfiles/js/filter.js
+++ staticfiles/js/filter.js
@@ -1,44 +1,103 @@
-// Persist the selected value of the temperature selector filter.
+// Persist the selected value of the temperature and diet filter.
 // Code based on `themes.js` in this repository.
 
-// get the tenperature & diet selector dropdown form
+// get the temperature & diet selector dropdown forms
 let temperatureSelector = document.getElementById('temperature-selector');
 let dietSelector = document.getElementById('diet-selector');
-
+let tasteSelector = document.getElementById('taste-selector');
+let textureSelector = document.getElementById('texture-selector');
 
 /** populate storage with the value form the selector form */
 function storeFilters() {
+
     localStorage.setItem("temperatureFilter", temperatureSelector.value);
     localStorage.setItem("dietFilter", dietSelector.value);
+    localStorage.setItem("tasteFilter", tasteSelector.value);
+    localStorage.setItem("textureFilter", textureSelector.value);
     
     applyFilters();
 }
 
 /** filter recipes based on the value in storage */
 function applyFilters() {
+
     // get the filter value from storage
     let currentTemparatureFilter = localStorage.getItem("temperatureFilter");
     let currentDietFilter = localStorage.getItem("dietFilter");
+    let currentTasteFilter = localStorage.getItem("tasteFilter");
+    let currentTextureFilter = localStorage.getItem("textureFilter");
 
     // set the filter selector value to the one retrieved from storage
     temperatureSelector.value = currentTemparatureFilter;
     dietSelector.value = currentDietFilter;
+    tasteSelector.value = currentTasteFilter;
+    textureSelector.value = currentTextureFilter;
 }
 
+/** reset the temperature filter form and storage value*/
 function resetTemperature() {
-    console.log("reset temperature")
-    localStorage.setItem("temperatureFilter", "any temperature");
     temperatureSelector.value = "any temperature";
+    localStorage.setItem("temperatureFilter", "any temperature");
 }
 
+/** reset the diet filter form and storage value*/
 function resetDiet() {
-    console.log("reset diet")
-    localStorage.setItem("dietFilter", "any diet");
     dietSelector.value = "any diet";
+    localStorage.setItem("dietFilter", "any diet");
+}
+
+/** reset the taste filter form and storage value*/
+function resetTaste() {
+    tasteSelector.value = "any taste";
+    localStorage.setItem("tasteFilter", "any taste");
+}
+
+/** reset the texture filter form and storage value*/
+function resetTexture() {
+    textureSelector.value = "any texture";
+    localStorage.setItem("textureFilter", "any texture");
+}
+
+/** if the URL doesn't include the string 'filter', reset all filters */
+function resetAllFilters() {
+    if (!window.location.href.includes('filter')) {
+        console.log("reset all filters")
+
+        localStorage.setItem("temperatureFilter", "any temperature");
+        temperatureSelector.value = "any temperature";
+
+        localStorage.setItem("dietFilter", "any diet");
+        dietSelector.value = "any diet";
+
+        localStorage.setItem("tasteFilter", "any taste");
+        tasteSelector.value = "any taste";
+        
+        localStorage.setItem("textureFilter", "any texture");
+        textureSelector.value = "any texture";
+    }
 }
 
 temperatureSelector.addEventListener("change", storeFilters);
-dietSelector.addEventListener("change", storeFilters);
 temperatureSelector.addEventListener("change", resetDiet);
+temperatureSelector.addEventListener("change", resetTaste);
+temperatureSelector.addEventListener("change", resetTexture);
+
+dietSelector.addEventListener("change", storeFilters);
 dietSelector.addEventListener("change", resetTemperature);
+dietSelector.addEventListener("change", resetTaste);
+dietSelector.addEventListener("change", resetTexture);
+
+
+tasteSelector.addEventListener("change", storeFilters);
+tasteSelector.addEventListener("change", resetTemperature);
+tasteSelector.addEventListener("change", resetDiet);
+tasteSelector.addEventListener("change", resetTexture);
+
+textureSelector.addEventListener("change", storeFilters);
+textureSelector.addEventListener("change", resetTemperature);
+textureSelector.addEventListener("change", resetDiet);
+textureSelector.addEventListener("change", resetTaste);
+
+
 document.addEventListener("DOMContentLoaded", applyFilters)
+document.addEventListener("DOMContentLoaded", resetAllFilters);
