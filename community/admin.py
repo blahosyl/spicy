da0cc03991477bbdd95498239fda3e62dfa5c1df
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-06-20 04:34:38 +0000

    restrict staff users from other's profiles in admin interface #42

--- community/admin.py
+++ community/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Profile
-from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 
@@ -8,3 +7,11 @@ from django_summernote.admin import SummernoteModelAdmin
 class ProfileAdmin(admin.ModelAdmin):
 
     prepopulated_fields = {"slug": ("user",)}
+
+    # staff users (who are not superadmins) should only view, change & delete their own profiles
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
+
