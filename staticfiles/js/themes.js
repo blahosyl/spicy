e7ca11c4cfa9a865faabddae1a71c12629057a37
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-06-24 18:48:04 +0000

    collect static

--- staticfiles/js/themes.js
+++ staticfiles/js/themes.js
@@ -15,40 +15,33 @@
 const bodyStyles = document.body.style;
 // get the color theme selector dropdown form
 let themeSelector = document.getElementById('theme-selector');
-
-/** check if storage is already populated */
-function checkStorage() {
-    if(localStorage.getItem('colorTheme') = '') {
-        localStorage.setItem("colorTheme", 'orange');
-        themeSelector.value = 'orange';
-        themeSelector.innerHTML = 'orange';
-        console.log('defaulting to orange')
-        storeColorTheme();
-    } else {
-        setColorTheme();
-    }
-}
+// get the locally stored color theme value (if any)
+let currentTheme = localStorage.getItem("colorTheme")
 
 /** populate storage with the value form the color theme selector form */
 function storeColorTheme() {
+    console.log("storeColorTheme");
     localStorage.setItem("colorTheme", themeSelector.value);
+    console.log(localStorage.getItem("colorTheme"));
     
     setColorTheme();
 }
 
 /** set the site's color theme based on the value in storage */
 function setColorTheme() {
-    // get the theme value from storage
-    let currentTheme = localStorage.getItem("colorTheme");
-
-    if (!currentTheme) {
-        themeSelector.value = 'orange'
+    console.log("setColorTheme");
+    // if there is no stored value, default to orange
+    if (!localStorage.getItem("colorTheme")) {
+        console.log("setColorTheme: default to orange");
+        currentTheme = 'orange';
+    } else {
+        // get the theme value from storage
+        console.log("setColorTheme: get value from storage");
+        currentTheme = localStorage.getItem("colorTheme");
     }
-    else {
-        // set the theme selector value to the one retrieved from storage
-        themeSelector.value = currentTheme
-    };
 
+    // set the theme selector value to the one retrieved from storage
+    themeSelector.value = currentTheme;
 
     // set the CSS color variables depending on the selected theme
 
@@ -68,5 +61,5 @@ function setColorTheme() {
     }
 }
 
-themeSelector.addEventListener("change", checkStorage);
-document.addEventListener("DOMContentLoaded", checkStorage);
+themeSelector.addEventListener("change", storeColorTheme);
+document.addEventListener("DOMContentLoaded", setColorTheme);
